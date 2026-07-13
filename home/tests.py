63bec5526458bee82b1cf82a40fe86e10cce725a
Chris Kalos <git@chriskalos.xyz>
2026-07-13 16:48:21 +0300

    Add homepage product list

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -11,4 +13,22 @@ class HomePageTests(TestCase):
         self.assertTemplateUsed(response, "home/base.html")
         self.assertTemplateUsed(response, "home/includes/header.html")
         self.assertTemplateUsed(response, "home/includes/footer.html")
-        self.assertContains(response, "Hello World!")
+
+    def test_home_page_displays_products(self):
+        response = self.client.get(reverse("home"))
+
+        products = response.context["products"]
+        self.assertEqual(len(products), 4)
+        self.assertEqual(
+            products[0],
+            {
+                "image": None,
+                "title": "Madeon - Victory LP",
+                "description": "Placeholder description for Madeon - Victory LP",
+                "price": Decimal("69.99"),
+            },
+        )
+        self.assertContains(response, "Madonna - Confessions II LP")
+        self.assertContains(response, "Porter Robinson - Smile LP")
+        self.assertContains(response, "Secret Unreleased Daft Punk LP")
+        self.assertContains(response, "420.00€")
--- home/views.py
+++ home/views.py
@@ -1,5 +1,34 @@
+from decimal import Decimal
+
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'home/index.html')
+    products = [
+        {
+            "image": None,
+            "title": "Madeon - Victory LP",
+            "description": "Placeholder description for Madeon - Victory LP",
+            "price": Decimal("69.99"),
+        },
+        {
+            "image": None,
+            "title": "Madonna - Confessions II LP",
+            "description": "Placeholder",
+            "price": Decimal("69.99"),
+        },
+        {
+            "image": None,
+            "title": "Porter Robinson - Smile LP",
+            "description": "Placeholder",
+            "price": Decimal("49.99"),
+        },
+        {
+            "image": None,
+            "title": "Secret Unreleased Daft Punk LP",
+            "description": "Aliens",
+            "price": Decimal("420.00"),
+        },
+    ]
+
+    return render(request, "home/index.html", {"products": products})
