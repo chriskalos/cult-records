f4052c1e10909c67945ea8bf543d00dcf26c88ea
Chris Kalos <git@chriskalos.xyz>
2026-07-13 16:19:56 +0300

    Add reusable home page layout

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_home_page_uses_reusable_layout(self):
+        response = self.client.get(reverse("home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/index.html")
+        self.assertTemplateUsed(response, "home/base.html")
+        self.assertTemplateUsed(response, "home/includes/header.html")
+        self.assertTemplateUsed(response, "home/includes/footer.html")
+        self.assertContains(response, "Hello World!")
